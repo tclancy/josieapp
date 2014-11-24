5e77cddf11d05f812f3f03c75c607aac2933af77
Tom Clancy <tclancy@gmail.com>
2014-11-24 16:25:47 -0500

    Fooling around with buttons

---
+++ app.js
@@ -0,0 +1,8 @@
+var React = require('react'),
+  mui = require('material-ui'),
+  PaperButton = mui.PaperButton;
+
+React.render(
+    <PaperButton type="FAB" icon="action-grade" />,
+    document.getElementById('envelope')
+);
